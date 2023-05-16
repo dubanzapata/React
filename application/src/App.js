a70a846da69da95ace6d18a730f0fb7307f12331
”duban” <Duban.Zapata@teaminternational.com>
2023-05-15 22:14:04 -0500

    propiedades por defecto

--- application/src/App.js
+++ application/src/App.js
@@ -1,42 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import Componente from './components/Componente';
+import Propiedades from './components/Propiedades';
 function App() {
-  let nombre="duban";
-  let auth=false;
-  let arreglo=["Primavera","Verano","Otoño"];
-
+  
   return (
     <div className="App">
+
       <header className="App-header">
+        <section>
         <img src={logo} className="App-logo" alt="logo" />
 
-        <label htmlFor="nombre">Nombre</label>
-        <input type="text" id="nombre"/>
-        <p>{auth?"el usuario esta logueado":"el usurio no esta logueado"}</p>
-        <p>{2+1}</p>
-        
-        <ul>
-          {arreglo.map((e,index)=>(
-          <li key={index}>{e}</li>
-        ))}</ul>
+<p>
+  Edit <code>src/App.js</code> and save to reload.
+</p>
+<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a>
+        </section>
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <section>
+        <section>
         <Componente msg="hola soy un componente funcional expresado"/>
-      </section>
-
+        <hr/>
+        <Propiedades></Propiedades>
+      </section>  
+     
+      </header>
     </div> 
   );
 }
---
+++ application/src/components/Propiedades.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Propiedades (props){
+
+    return (
+        <div>
+        <h2>{props.porDefecto} </h2>
+    </div>
+    )
+    
+    
+}
+
+Propiedades.defaultProps={
+    porDefecto: "propiedad"
+}
+
